3eb56499493cf869b7e3468295a05d9280bcf719
Truchon <katie.truchon@gmail.com>
2024-09-05 10:53:31 -0400

    w8d2 - in class update

---
+++ week8_inclass/loops.py
@@ -0,0 +1,10 @@
+people = ["Katie", "Ace", "Ian", "Jose", "Cullen"]
+color = ["grey", "blue", "black", "green", "purple"]
+
+i = 0 
+peopleLength = len(people)
+
+while i < peopleLength:
+   
+    print(f"INDEX: ${i}\n${people[i]}'s favorite color is ${color[i]}")
+    i = i + 1
